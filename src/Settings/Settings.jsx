fd35fd5ab9f5dab1d42623a191c7831b790b3087
shadycodee <mikojohn158@gmail.com>
2024-09-25 21:21:05 +0800

    settings form

--- src/Settings/Settings.jsx
+++ src/Settings/Settings.jsx
@@ -1,45 +1,44 @@
-import React from 'react'
-import { CssVarsProvider, extendTheme } from '@mui/joy/styles';
-import { useNavigate } from 'react-router-dom';
-import Sheet from '@mui/joy/Sheet';
-import Box from '@mui/joy/Box';
-import Button from '@mui/joy/Button';
-import Dropdown from '@mui/joy/Dropdown';
-import IconButton from '@mui/joy/IconButton';
-import ListDivider from '@mui/joy/ListDivider';
-import ListItemDecorator from '@mui/joy/ListItemDecorator';
-import Menu from '@mui/joy/Menu';
-import MenuButton from '@mui/joy/MenuButton';
-import MenuItem from '@mui/joy/MenuItem';
-import Typography from '@mui/joy/Typography';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import InsightsTwoTone from '@mui/icons-material/InsightsTwoTone';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Sheet from "@mui/joy/Sheet";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Dropdown from "@mui/joy/Dropdown";
+import ListDivider from "@mui/joy/ListDivider";
+import ListItemDecorator from "@mui/joy/ListItemDecorator";
+import Menu from "@mui/joy/Menu";
+import MenuButton from "@mui/joy/MenuButton";
+import MenuItem from "@mui/joy/MenuItem";
+import Typography from "@mui/joy/Typography";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import { Container, TextField } from "@mui/material";
+import "./Settings.css";
 
-const Settings = () =>  {
-  const navigate = useNavigate()
-  
+const Settings = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="container">
         <Sheet variant="outlined" className="sheet">
           {/* Header Section */}
-          <Sheet variant="solid"
+          <Sheet
+            variant="solid"
             className="header"
-            sx={{ 
-              backgroundColor: '#ffd000', 
-              borderRadius: '0',
-              marginTop: '0',
-              top: 0, 
+            sx={{
+              backgroundColor: "#ffd000",
+              borderRadius: "0",
+              marginTop: "0",
+              top: 0,
               left: 0,
               zIndex: 1000,
             }}
           >
-          <Box className="logo" />
-          <Typography
+            <Box className="logo" />
+            <Typography
               component="div"
-              sx={{ marginLeft: '16px', textAlign: 'left', fontSize: '20px' }}
+              sx={{ marginLeft: "16px", textAlign: "left", fontSize: "20px" }}
             >
               <div>LIC Connect</div>
               <div>Library Internet Center</div>
@@ -50,29 +49,31 @@ const Settings = () =>  {
                 <MenuButton
                   className="menu-Button"
                   sx={{
-                    '--Button-radius': '1.5rem',
-                    backgroundColor: '#89343b',
-                    color: 'white',
-                    fontSize: '12px',
-                    '&:hover': {
-                      color: '#89343b',
-                      backgroundColor: 'white',
+                    "--Button-radius": "1.5rem",
+                    backgroundColor: "#89343b",
+                    color: "white",
+                    fontSize: "12px",
+                    "&:hover": {
+                      color: "#89343b",
+                      backgroundColor: "white",
                     },
                   }}
-                  variant= "outlined"
+                  variant="outlined"
                   endDecorator={<KeyboardArrowDownIcon />}
-                >Settings</MenuButton>
+                >
+                  Settings
+                </MenuButton>
                 <Menu
                   variant="outlined"
                   placement="bottom-start"
                   disablePortal
                   size="sm"
                   sx={{
-                    '--ListItemDecorator-size': '24px',
-                    '--ListItem-minHeight': '40px',
-                    '--ListDivider-gap': '4px',
+                    "--ListItemDecorator-size": "24px",
+                    "--ListItem-minHeight": "40px",
+                    "--ListDivider-gap": "4px",
                     minWidth: 200,
-                    fontSize: '12px'
+                    fontSize: "12px",
                   }}
                 >
                   <MenuItem>
@@ -82,24 +83,67 @@ const Settings = () =>  {
                     Account
                   </MenuItem>
                   <ListDivider />
-                  <MenuItem onClick={() => navigate('/dashboard')}>Dashboard</MenuItem>
+                  <MenuItem onClick={() => navigate("/dashboard")}>
+                    Dashboard
+                  </MenuItem>
                   <ListDivider />
-                  <MenuItem onClick={() => navigate('/staff')}>Manage Staff</MenuItem>
+                  <MenuItem onClick={() => navigate("/staff")}>
+                    Manage Staff
+                  </MenuItem>
                 </Menu>
               </Dropdown>
             </Box>
-            
-      
           </Sheet>
-          
+          <Container maxWidth="sm" className="passwordForm">
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: 2,
+                mt: 4,
+              }}
+            >
+              <Typography variant="h5" component="h2" gutterBottom>
+                Change Password
+              </Typography>
+              <TextField
+                label="Current Password"
+                variant="outlined"
+                type="password"
+                fullWidth
+                required
+              />
+              <TextField
+                label="New Password"
+                variant="outlined"
+                type="password"
+                fullWidth
+                required
+              />
+              <TextField
+                label="Confirm Password"
+                variant="outlined"
+                type="password"
+                fullWidth
+                required
+              />
+              <Button
+                variant="contained"
+                color="success"
+                fullWidth
+                sx={{ mt: 2 }}
+              >
+                Change Password
+              </Button>
+            </Box>
+          </Container>
         </Sheet>
-        <Sheet>
+      </div>
 
-        </Sheet>
-     </div>
-
+      {/* Change Password Form */}
     </>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;
