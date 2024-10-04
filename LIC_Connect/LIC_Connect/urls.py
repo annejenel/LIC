1292460c3161edf97dff26e66e64644b41077d79
Anne <annejilosorio@gmail.com>
2024-10-04 13:31:34 +0800

    manage staff

--- LIC_Connect/LIC_Connect/urls.py
+++ LIC_Connect/LIC_Connect/urls.py
@@ -14,11 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from students.views import StudentViewSet, StaffViewSet
+
+
+router = DefaultRouter()
+router.register(r'staff', StaffViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('students.urls')),  # Add this line
+    path('api/', include('students.urls')),  
+    path('api/', include(router.urls)),
 ]
